1f2e1f163bb55e1ba076df11f45355156f8a3576
fernando.js8 <fernando.js8@outlook.com>
2021-06-02 20:55:08 -0300

    start

---
+++ CMDemo/WPF.UI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.UI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private string _firstName = "Fernando";
+
+        public string FirstName
+        {
+            get 
+            { 
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+
+    }
+}
